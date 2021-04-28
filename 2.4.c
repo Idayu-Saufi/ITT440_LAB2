9f30a63f3e0d3485d0f98eee22e8e7f6961c8263
Idayu-Saufi <sitinoridayu149@gmail.com>
2021-04-28 07:01:34 +0000

    Question 2.4

--- 2.4.c
+++ 2.4.c
@@ -5,35 +5,35 @@
 #include <unistd.h>
 
 
-void childTask() {
+void childTask() {	//function for childTask
   printf("\nGood Day, What should I call you? \n");
 }
 
-void parentTask() {
+void parentTask() {	//function for parentTask
   printf("Job is done \n");
 }
 
 int main() {
-  for(int i = 1; i < 5; i++) {
-    pid_t pid = fork();
+  for(int i = 1; i < 5; i++) {	//to loop 4-child process
+    pid_t pid = fork();	//using fork()
     char name[20];
 
 
     if(pid == 0) {
      childTask();
      printf("Name: ");
-     fgets(name, sizeof(name), stdin);
-     printf("\nHi! nice to meet you, %s",name);
+     scanf("%s",name);	//ask user to enter name
+     printf("\nHi! glad to know you, %s.\n",name);	//display name given by user
      printf("\nChild process => PPID=%d, PID=%d\n", getppid(), getpid());
      exit(0);
     }
     else {
      printf("Parent process => PID=%d\n", getpid());
      printf("Waiting for child processes to finish... \n\n");
-     wait(NULL);
+     wait(NULL);	//using wait()
     } 
    }
 
-   parentTask();
+   parentTask();	//to print out Job is done at the end of process
    return EXIT_SUCCESS;
   }
